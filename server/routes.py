6ae77b1ad880ffe538ec4e2e46459d27d7a76d7e
EC2 Default User <ec2-user@ip-172-31-47-229.us-east-2.compute.internal>
2020-09-19 21:34:31 +0000

    Jsonfiy products route return

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,6 @@
 from server import app, db
 from server.models import ProductScrape
-from flask import request, json
+from flask import request, jsonify
 
 @app.route('/')
 @app.route('/index')
@@ -15,7 +15,9 @@ def not_found():
 
 @app.route('/scrape')
 def scrape():
-    ProductScrape(part_id=2, vendor_id='1A', scraped_url='https://www.company.com', raw_text='specs specs more specs').save()
-    scraped_specs = ProductScrape.objects.all()
-    return 'Scrape OK'
+    ProductScrape(part_id=5, vendor_id='1A', scraped_url='https://www.company.com', raw_text='specs specs more specs').save()
+    return 'OK'
 
+@app.route('/products')
+def products():
+    return jsonify(ProductScrape.objects.all())
