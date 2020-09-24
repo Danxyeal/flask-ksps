15428337f402c42e6b86fea3eef2858a015fe654
Daniel <daniel@kiwi-systems.com>
2020-09-24 03:21:11 +0000

    backapp

--- server/routes.py
+++ server/routes.py
@@ -5,19 +5,14 @@ from flask import request, jsonify
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello Flask'
-
-'''
-@app.route('*')
-def not_found():
-    return redirect(url_for('home'))
-    '''
+    return 'Hello Flask', 200
 
 @app.route('/scrape')
 def scrape():
     ProductScrape(part_id=5, vendor_id='1A', scraped_url='https://www.company.com', raw_text='specs specs more specs').save()
-    return 'OK'
+    return 'OK', 201
 
 @app.route('/products')
 def products():
-    return jsonify(ProductScrape.objects.all())
+    return jsonify(ProductScrape.objects.all()), 200
+
